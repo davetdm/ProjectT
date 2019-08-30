a898fda9d1514f2255ef59fcd96fe4af2e45e107
REAMANZINI <reamanzini@gmail.com>
2019-08-30 16:26:33 +0200

    made changes on the product  page and resized the images

--- application/views/product.php
+++ application/views/product.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+<html>
 <body style="background-color: #cccccc">
 
 <section class="container">
@@ -8,9 +8,6 @@
             <div class="col-md-7">
                 <h2>Choose A Color</h2>
                 <form id="frmCart" action="<?php echo ROOT_URL; ?>product/cart" method="post" class="form-area">
-=======
-<?php require_once "includes/header.php" ?>
-<?php require_once "includes/menu.php"?>
 
 <header class="masthead" style="background-image: url('<?php echo STATIC_URL; ?>/img/contact-bg.jpg')">
 
@@ -29,13 +26,11 @@
 
 <body>
 <main class="container">
->>>>>>> c65233ec4c244f782384147df8093dce65d4fb78
     <!--  Heels Image -->
     <div class="left-column">
-        <img data-image="maroon" src="<?php echo STATIC_URL; ?>images/23.jpg" style="width:200px; height:121px;" alt="">
-        <img data-image="blue" src="<?php echo STATIC_URL; ?>images/25.jpg" style="width:200px; height:121px;" alt="">
-        <img data-image="red" class="active" src="<?php echo STATIC_URL; ?>images/24.jpg" style="width:200px; height:121px;" alt="">
-
+        <img data-image="maroon" src="<?php echo STATIC_URL; ?>images/23.jpg" style="..." alt="">
+        <img data-image="blue" src="<?php echo STATIC_URL; ?>images/25.jpg" style="..." alt="">
+        <img data-image="red" class="active" src="<?php echo STATIC_URL; ?>images/24.jpg" style="..." alt="">
     </div>
     <div class="right-column">
         <!--  Heel description -->
@@ -59,7 +54,7 @@
                         <label for="blue"><span></span></label>
                     </div>
                     <div>
-                        <input data-image="maroon" type="radio" id="maroon" name="color" value="black">
+                        <input data-image="maroon" type="radio" id="maroon" name="color" value="maroon">
                         <label for="maroon"><span></span></label>
                     </div>
                 </div>
@@ -69,26 +64,17 @@
         <!-- Product Price -->
         <div class="product-price">
             <span>R1200</span>
-            <div> <button type="submit" id="btnSend" name="submit" class="btn btn-primary btn-lg" required="required">Add To Cart</button></div>
+            <div> <button type="submit" id="btnCart" name="submit" class="btn btn-primary btn-lg" required="required">Add To Cart</button></div>
         </div>
-<<<<<<< HEAD
     </div
-                </form>
-
-            </div>
-        </div>
-        <!--/.row-->
+ </form>
+</div>
+</div>
     </div>
-
     <script src="<?php echo STATIC_URL?>https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js" charset="utf-8"></script>
     <script src="<?php echo STATIC_URL?>script.js" charset="utf-8"></script>
 
 </section>
 
 </body>
-=======
-    </div>
-</main>
-</body>
-<?php require_once "includes/footer.php" ?>
->>>>>>> c65233ec4c244f782384147df8093dce65d4fb78
+</html>
